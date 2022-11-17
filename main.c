94e9e4abbc239d7461a0341f4309c194064630de
adnan-erdogan <adnan.erdogan@akinon.com>
2022-11-17 12:31:45 +0300

    Add language properties example file, show contents function, modified Makefile to standart version

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
+
+#define BUF_SIZE 1024
+
+
+void show_contents(char *contents, const char delimiter) {
+    char c;
+    for (int i = 0; i < strlen(contents); i++) {
+        c = contents[i];
+        if(c == '\n')
+            printf("\n");
+        else
+            printf("%c%c", delimiter, c);
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    printf("hello world\n");
+    char buf[BUF_SIZE];
+    FILE *fp = fopen("main.wise", "r");
+    fread(buf, BUF_SIZE, 1, fp);
+    /** printf("%s\n", buf); */
+    show_contents(buf, '`');
+    fclose(fp);
     return 0;
 }
